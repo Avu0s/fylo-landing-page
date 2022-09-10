db20767afdf6a1a6bcb4825a33d472f7ed09dc76
Avus <avuwake@gmail.com>
2022-09-10 18:39:20 +0100

    socials

---
+++ app.js
@@ -0,0 +1,5 @@
+'use strict'
+const inputOne = document.querySelector('#input-one')
+const inputTwo = document.querySelector('#input-two')
+const btnOne = document.querySelector('#btn-one')
+const btnTwo = document.querySelector('#btn-two')
